0ef1713810ecd6f2d37953de7fd303dbdbaf3e93
Ilan Elfen <u24ielfen@wbais.net>
2022-12-20 21:47:43 +0200

    another|

--- src/Major/Home.js
+++ src/Major/Home.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../Styles/home.css";
 function Home() {
+  const [teamsList, setTeamsList] = useState([]);
+  useEffect(() => {
+    const getTeamStats = async () => {
+      const { data: post } = await Axios.get(
+        `https://https://scouting2.herokuapp.com/getTeams`
+      );
+      setTeamsList(post);
+    };
+    getTeamStats();
+  }, []);
   return (
     <div>
       <div className="container teams ">
         <br />
         <div className="headers">Teams</div>
+        {teamsList.map((value, key) => {
+          return <>{value.team_name}</>;
+        })}
       </div>
     </div>
   );
